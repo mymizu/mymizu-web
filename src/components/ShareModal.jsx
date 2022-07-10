36cb985c80458c5dcde3cdc875c2572ad627d966
Dominic Lowes <chaiskeng@gmail.com>
2022-07-10 16:30:48 +0900

    Feat: add copy and close buttons to share modal
    Co-authored-by: panzerstadt <panzerstadt@users.noreply.github.com>
    Co-authored-by: jmourell <jmourell@users.noreply.github.com>

--- src/components/ShareModal.jsx
+++ src/components/ShareModal.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export const ShareModal = ({ data }) => {
+export const ShareModal = ({ data, setShareModal }) => {
     const copyFunc = (data) => {
         navigator.clipboard.writeText(data.action.share);
         alert("Copied")
+    }
+    const closeFunc = () => {
+        setShareModal(false);
     }
     return (
         <div className='share-modal'>
@@ -11,6 +14,7 @@ export const ShareModal = ({ data }) => {
             <div className='share-link'>{data.title}</div>
             <div>{data.action.share ? data.action.share : "No Link, feel free to add one!"}</div>
             <button className='copy-button' onClick={copyFunc(data)}>Copy</button>
+            <button className='close-share-modal' onClick={closeFunc}>X</button>
         </div>
     )
 }
