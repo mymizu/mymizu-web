44ca6fec768660ed67bfcb533066c60ca84f88a2
juliwithoutthee <jnboucree@gmail.com>
2022-07-10 17:12:06 +0900

    Adds FunFacts to App

--- src/App.jsx
+++ src/App.jsx
@@ -196,6 +196,7 @@ export function App({ gmApiKey }) {
                   )
                 }
               </ul>
+              <FunFacts />
             </footer>
           </div>
         </div>
