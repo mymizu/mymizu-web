5cfd6e274177876e1ab8056e9757c6f8509f2fab
Dominic Lowes <chaiskeng@gmail.com>
2022-07-10 16:18:49 +0900

    Refactor: update share modal
    Co-authored-by: panzerstadt <panzerstadt@users.noreply.github.com>
    Co-authored-by: jmourell <jmourell@users.noreply.github.com>

--- src/components/ShareModal.jsx
+++ src/components/ShareModal.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 export const ShareModal = ({ data }) => {
+    const copyFunc = (data) => {
+        navigator.clipboard.writeText(data.action.share);
+        alert("Copied")
+    }
     return (
         <div className='share-modal'>
             <a>Share this Refill spot</a>
             <div className='share-link'>{data.title}</div>
             <div>{data.action.share ? data.action.share : "No Link, feel free to add one!"}</div>
+            <button className='copy-button' onClick={copyFunc(data)}>Copy</button>
         </div>
     )
 }
