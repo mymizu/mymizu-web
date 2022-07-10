800571c42c8bbd0259b21115471e44acfd66daf7
Neo <w-masawat@ispace-inc.com>
2022-07-10 07:35:36 +0000

    Neo: added language switcher into the hamburger
    Signed-off-by: Neo <w-masawat@ispace-inc.com>

---
+++ src/components/statistics.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+export function Statistics() {
+    return (
+        <>
+            <div className="container-lg">
+                <div className="row home">
+                    <div className="col" id="forest">
+                        Column
+                    </div>
+                    <div className="col" id="money">
+                        Column
+                    </div>
+                    <div className="col" id="ocean">
+                        Column
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
