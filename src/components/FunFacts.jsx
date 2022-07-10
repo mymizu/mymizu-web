6781c0d560480afa6b1b7016c21b6301617859e1
juliwithoutthee <jnboucree@gmail.com>
2022-07-10 16:10:04 +0900

    Creates an array for emojis

--- src/components/FunFacts.jsx
+++ src/components/FunFacts.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { FormattedMessage } from "react-intl";
 
 export function FunFacts() {
 
+    const funEmoji = ["💧", "🌊", "⏳", "🛍", "🥽", "🗑", "🏝", "🌏", "😯", "♳", "🐟"];
 
     return (
         <>
-            <div className="funFacts">
+            <div className="facts">
 
+                <FormattedMessage id={funFacts[currentIndex].fact} />
             </div>
-
 
         </>
     )
