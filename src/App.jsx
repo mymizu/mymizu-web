19c3b6c0bf3fd528335c0d314750124a8238ca3a
Andrew Park <atpark333@gmail.com>
2022-07-10 13:17:02 +0900

    Add IntlProvider and i18n config to root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from "react";
 import GoogleMapReact from "google-map-react";
 import axios from "axios";
+import { IntlProvider }  from "react-intl";
+
+const translations = {
+  en: require("./translations/en.json"),
+  ja: require("./translations/ja.json")
+};
+const i18nConfig = {
+  // TODO: locale should be stored in state and use users's locale by default
+  locale: "ja",
+  defaultLocale: "ja",
+  messages: translations["ja"]
+};
 
 const Marker = () => <div className="marker"><img className="pin" src="/public/images/map-pin.svg" /></div>;
 
@@ -23,11 +35,11 @@ export function App({ gmApiKey }) {
   }
 
   const topNav = [
-    { href: "#", title: "給水MAPの使い方" },
-    { href: "#", title: "mymizuについて" },
-    { href: "#", title: "コミュニティに参加" },
-    { href: "#", title: "mymizuについて" },
-    { href: "#", title: "お店にmymizuを紹介" },
+    { id: "topnav.map", ref: "#", title: "給水MAPの使い方" },
+    { id: "topnav.about", ref: "#", title: "mymizuについて" },
+    { id: "topnav.community", ref: "#", title: "コミュニティに参加" },
+    { id: "topnav.about", ref: "#", title: "mymizuについて" },
+    { id: "topnav.partners", ref: "#", title: "お店にmymizuを紹介" },
   ]
 
   const socialNav = [
@@ -66,93 +78,95 @@ export function App({ gmApiKey }) {
   }, [taps, setInitialLoad, initialLoad, setTaps]);
 
   return (
-    <div>
-      {/* Header */}
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid nav-container">
-          <img id="logo" src="/public/images/logo.svg" alt="" height="45" className="d-inline-block align-text-top" />
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul className="nav" style={{ marginTop: 10 }}>
-              {
-                topNav.map((el, i) =>
-                  <li className="nav-item" key={i}>
-                    <a className="nav-link" href={el.href}>{ el.title }</a>
-                  </li>
-                )
-              }
-              <li className="nav-item lang-selector">
-                <a className="nav-link" href="#">JP</a> | <a className="nav-link" href="#">EN</a>
-              </li>
-            </ul>
+    <IntlProvider messages={i18nConfig.messages} locale={i18nConfig.locale} defaultLocale={i18nConfig.defaultLocale}>
+      <div>
+        {/* Header */}
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid nav-container">
+            <img id="logo" src="/public/images/logo.svg" alt="" height="45" className="d-inline-block align-text-top" />
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <ul className="nav" style={{ marginTop: 10 }}>
+                {
+                  topNav.map((el, i) =>
+                    <li className="nav-item" key={i}>
+                      <a className="nav-link" href={el.href}>{ el.title }</a>
+                    </li>
+                  )
+                }
+                <li className="nav-item lang-selector">
+                  <a className="nav-link" href="#">JP</a> | <a className="nav-link" href="#">EN</a>
+                </li>
+              </ul>
+            </div>
+            <span className="hamburger d-sm-inline d-md-inline d-lg-none" onClick={handleNav}>&#9776;</span>
           </div>
-          <span className="hamburger d-sm-inline d-md-inline d-lg-none" onClick={handleNav}>&#9776;</span>
+          <div className="overlay" style={{
+            left: navOpen ? "0%" : "-100%",
+          }}>
+            <div className="overlay-content">
+              <span className="closebtn" onClick={handleNav} >&times;</span>
+              <div className="nav-container">
+                { topNav.map((el, i) => <a href={el.href} key={i}>{el.title}</a>) }
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <div style={{ height: "70vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: gmApiKey }}
+            defaultCenter={gmDefaultProps.center}
+            defaultZoom={gmDefaultProps.zoom}>
+            { !loading && taps.length ? taps.map((tap) =>
+              <Marker key={tap.id} lat={tap.latitude} lng={tap.longitude} />
+            ) : null }
+          </GoogleMapReact>
         </div>
-        <div className="overlay" style={{
-          left: navOpen ? "0%" : "-100%",
-        }}>
-          <div className="overlay-content">
-            <span className="closebtn" onClick={handleNav} >&times;</span>
-            <div className="nav-container">
-              { topNav.map((el, i) => <a href={el.href} key={i}>{el.title}</a>) }
+
+        <div className="container-lg">
+          <div className="row home">
+            <div className="col" id="forest">
+              Column
+            </div>
+            <div className="col" id="money">
+              Column
+            </div>
+            <div className="col" id="ocean">
+              Column
             </div>
           </div>
         </div>
-      </nav>
 
-      <div style={{ height: "70vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: gmApiKey }}
-          defaultCenter={gmDefaultProps.center}
-          defaultZoom={gmDefaultProps.zoom}>
-          { !loading && taps.length ? taps.map((tap) =>
-            <Marker key={tap.id} lat={tap.latitude} lng={tap.longitude} />
-          ) : null }
-        </GoogleMapReact>
-      </div>
-
-      <div className="container-lg">
-        <div className="row home">
-          <div className="col" id="forest">
-            Column
-          </div>
-          <div className="col" id="money">
-            Column
-          </div>
-          <div className="col" id="ocean">
-            Column
+        <div className="footer">
+          <div className="container-lg">
+          <footer>
+            <ul className="nav justify-content-center">
+              {
+                socialNav.map((el, i) =>
+                  <li className="nav-item" key={i}>
+                    <a href={el.href} className="nav-link px-2 text-muted">
+                      <i className={`bi ${el.iconName}`} />
+                    </a>
+                  </li>
+                )
+              }
+            </ul>
+            <ul className="nav justify-content-center">
+              {
+                footerNav.map((el, i) =>
+                  <li className="nav-item" key={i}>
+                    <a href={el.href} className="nav-link px-2">
+                      { el.title }
+                    </a>
+                  </li>
+                )
+              }
+            </ul>
+          </footer>
           </div>
         </div>
-      </div>
 
-      <div className="footer">
-        <div className="container-lg">
-        <footer>
-          <ul className="nav justify-content-center">
-            {
-              socialNav.map((el, i) =>
-                <li className="nav-item" key={i}>
-                  <a href={el.href} className="nav-link px-2 text-muted">
-                    <i className={`bi ${el.iconName}`} />
-                  </a>
-                </li>
-              )
-            }
-          </ul>
-          <ul className="nav justify-content-center">
-            {
-              footerNav.map((el, i) =>
-                <li className="nav-item" key={i}>
-                  <a href={el.href} className="nav-link px-2">
-                    { el.title }
-                  </a>
-                </li>
-              )
-            }
-          </ul>
-        </footer>
-        </div>
       </div>
-
-    </div>
+    </IntlProvider>
   )
 }
