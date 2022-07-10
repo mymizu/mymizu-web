e252d2bf90009af7b15a3912b14d8fc2b6d537ae
juliwithoutthee <jnboucree@gmail.com>
2022-07-10 17:13:28 +0900

    Updates FunFacts with useEffect for randomization

--- src/components/FunFacts.jsx
+++ src/components/FunFacts.jsx
@@ -3,12 +3,24 @@ import { FormattedMessage } from "react-intl";
 
 export function FunFacts() {
 
+    const [randomKey, setRandomKey] = useState();
+
+    useEffect(() => {
+        const maxFactKey = 11;
+        setRandomKey(Math.floor(Math.random() * maxFactKey + 1));
+    }, []);
     const funEmoji = ["💧", "🌊", "⏳", "🛍", "🥽", "🗑", "🏝", "🌏", "😯", "♳", "🐟"];
+
+    function randomEmoji() {
+        let randomIndex = Math.floor(Math.random() * funEmoji.length);
+        return funEmoji[randomIndex];
+    }
 
     return (
         <>
             <div className="facts">
-                <FormattedMessage id={funFacts[currentIndex].fact} />
+                {randomEmoji()}
+                <FormattedMessage id={`fact${randomKey}`} />
             </div>
 
         </>
