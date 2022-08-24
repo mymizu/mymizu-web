077d3b3a0c0e42d3e3930db81e8a93d58f3240a8
Rafael Harahap <rafaelharahap@Rafaels-MacBook-Pro.local>
2022-08-24 15:22:58 +0700

    1. modify style on class .maps-location-search-results-container  overflow-y: scroll; 2.Refactor from class to function in googlemap.js but it doesn't work yet

--- utils/googlemaps.js
+++ utils/googlemaps.js
@@ -1,6 +1,61 @@
+<<<<<<< HEAD
 function googleMapsAPI(map, maps) {
   const autoCompleteService = new maps.places.AutocompleteService();
   const placeService = new maps.places.PlacesService(map);
+=======
+// function googleMapsAPI(map, maps) {
+//   const autoCompleteService = new maps.places.AutocompleteService();
+//   const placeService = new maps.places.PlacesService(map);
+
+//   const search = (input, callback) => {
+//     autoCompleteService.getPlacePredictions(
+//       {
+//         input,
+//       },
+//       (predictions) => {
+//         if (!predictions) {
+//           callback([]);
+//           return;
+//         }
+//         Promise.all(
+//           predictions.map((prediction) => {
+//             return lookupPlace(prediction.place_id);
+//           })
+//         ).then(callback);
+//       }
+//     );
+//   };
+
+//   const lookupPlace = (placeId) => {
+//     new Promise((resolve, reject) => {
+//       placeService.getDetails(
+//         {
+//           placeId,
+//           fields: ["icon", "name", "formatted_address", "geometry.location"],
+//         },
+//         resolve
+//       );
+//     });
+//   };
+
+//   return {
+//     map,
+//     maps,
+//     search,
+//     lookupPlace,
+//   };
+// }
+
+// export default googleMapsAPI;
+
+class GoogleMapsAPI {
+  setGoogleAPIObject = (map, maps) => {
+    this.map = map;
+    this.maps = maps;
+    this.AutocompleteService = new maps.places.AutocompleteService();
+    this.PlaceService = new maps.places.PlacesService(map);
+  };
+>>>>>>> 1. modify style on class .maps-location-search-results-container  overflow-y: scroll; 2.Refactor from class to function in googlemap.js but it doesn't work yet
 
   const search = (input, callback) => {
     autoCompleteService.getPlacePredictions(
