4b7492120f6b56be36986bb6ff65116bb48e4878
Lawrie <lawrie@catefamily.co.uk>
2022-09-12 16:13:33 +0900

    Add feedback link

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -30,7 +30,7 @@ export const Details = ({ data }) => {
       {data.link && (
         <div className="detail-section">
           <img src="/public/images/globe.svg" alt="" />
-          <div><a href={data.link} target="_new">{data.link}</a></div>
+          <div><a href={data.link} target="_new" rel="noopener nofollow">{data.link}</a></div>
         </div>
       )}
       {data.address && (
@@ -41,11 +41,7 @@ export const Details = ({ data }) => {
       )}
       <div className="detail-section">
         <img src="/public/images/info.svg" alt="" />
-        <div>Report/Suggest change to details</div>
-      </div>
-      <div className="detail-section">
-        <img src="/public/images/edit.svg" alt="" />
-        <div>Feedback: {data.description.feedback}</div>
+        <div><a href={`https://docs.google.com/forms/d/e/1FAIpQLSeRNzWZhQ7jBGzZQOJS4sHt1s4MUR6cf2AinT5ujioLJChPYQ/viewform?usp=pp_url&entry.2092238618=${data.id}`} target="_new">Report/Suggest change to details</a></div>
       </div>
     </div>
   );
